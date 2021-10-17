cd53a4f05241962d604db0a9f52d186f8217c8ca
LinaJulianti <80867343+LinaJulianti@users.noreply.github.com>
2021-10-17 13:50:22 +0700

    Add files via upload

--- akademik.sql
+++ akademik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 15, 2021 at 03:18 PM
+-- Generation Time: Oct 17, 2021 at 08:48 AM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 7.3.29
 
@@ -32,18 +32,22 @@ CREATE TABLE `mahasiswa` (
   `nama` varchar(50) DEFAULT NULL,
   `jkel` varchar(10) DEFAULT NULL,
   `alamat` text DEFAULT NULL,
-  `tgllhr` date DEFAULT NULL
+  `tgllhr` date DEFAULT NULL,
+  `prodi` varchar(20) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `mahasiswa`
 --
 
-INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`) VALUES
-('MHS01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01'),
-('MHS02', 'Rita', 'P', 'SOLO', '1999-01-01'),
-('MHS03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11'),
-('MHS04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15');
+INSERT INTO `mahasiswa` (`nim`, `nama`, `jkel`, `alamat`, `tgllhr`, `prodi`) VALUES
+('MHS01', 'Siti Aminah', 'P', 'SOLO', '1995-10-01', NULL),
+('MHS02', 'Rita', 'P', 'SOLO', '1999-01-01', NULL),
+('MHS03', 'Amirudin', 'L', 'SEMARANG', '1998-08-11', NULL),
+('MHS04', 'Siti Maryam', 'P', 'JAKARTA', '1995-04-15', NULL),
+('MHS05', 'Lina Julianti', 'P', 'Sumbawa', '2000-07-05', 'Teknik Informatika'),
+('MHS06', 'Alby', 'L', 'Mataram', '2001-02-12', 'Teknik Elektro'),
+('MHS07', 'Fatimah', 'P', 'Bali', '1999-06-18', 'Farmasi');
 
 --
 -- Indexes for dumped tables
