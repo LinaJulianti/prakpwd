90492f5583e21cc131f804e49b0af1af24f50dc4
LinaJulianti <80867343+LinaJulianti@users.noreply.github.com>
2021-12-10 21:14:22 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,15 @@
+<?php
+
+$host="localhost";  //nama host
+$username="root"; //username
+$password="";  //Password (isi jika menggunakan password)
+$databasename="akademik"; // database dengan nama akademik
+
+//membuat koneksi
+$con=@mysqli_connect($host,$username,$password,$databasename);
+
+//mengecek koneksi
+if (!$con) {
+    echo "Error: " . mysqli_connect_error();
+    exit();
+}
